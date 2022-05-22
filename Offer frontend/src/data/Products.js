b4727439c0e911ada296f7fd4906347adc3f9087
it20276614 <73341274+it20276614@users.noreply.github.com>
2022-05-22 15:19:49 +0530

    Add files via upload

---
+++ Offer frontend/src/data/Products.js
@@ -0,0 +1,70 @@
+const products = [
+  {
+    _id: "1",
+    name: "THE HOBBIT",
+    image: "/images/15.jpg",
+    description:
+      "The Hobbit, or There and Back Again is a children's fantasy novel by English author J. R. R. Tolkien. It was published in 1937 to wide critical acclaim, being nominated for the Carnegie Medal and awarded a prize from the New York Herald Tribune for best juvenile fiction.",
+    price: 145,
+    countInStock: 3,
+    rating: 4,
+    numReviews: 4,
+  },
+  {
+    _id: "2",
+    name: "Velcro Sneakers For Boys & Girls  (Blue)",
+    image: "/images/27.jpg",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    price: 599,
+    countInStock: 10,
+    rating: 2,
+    numReviews: 2,
+  },
+  {
+    _id: "3",
+    name: "Sesame Street Unisex-Child ELMO Puppet Slipper",
+    image: "/images/17.jpg",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    price: 929,
+    countInStock: 0,
+    rating: 3.5,
+    numReviews: 3,
+  },
+  {
+    _id: "4",
+    name: "Lace Casual Boots For Boys & Girls  (Tan)",
+    image: "/images/18.jpg",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    price: 399,
+    countInStock: 10,
+    rating: 5,
+    numReviews: 9,
+  },
+  {
+    _id: "5",
+    name: "Lace Walking Shoes For Boys & Girls  (Pink)",
+    image: "/images/19.jpg",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    price: 49,
+    countInStock: 7,
+    rating: 2,
+    numReviews: 2,
+  },
+  {
+    _id: "6",
+    name: "Women Red Heels Sandal",
+    image: "/images/20.jpg",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    price: 29,
+    countInStock: 0,
+    rating: 0,
+    numReviews: 0,
+  },
+];
+
+export default products;
